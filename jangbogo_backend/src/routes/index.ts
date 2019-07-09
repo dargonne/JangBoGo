d51ae1ede3d8b5332a611e4ba344fff710da05e8
Y2dgWLzz1T <seungjin.public@gmail.com>
2019-07-09 20:31:20 +0900

    REST API 라우팅 취합

--- jangbogo_backend/src/routes/index.ts
+++ jangbogo_backend/src/routes/index.ts
@@ -1,8 +1,16 @@
+/** 
+ * @project  "JANGBOGO"
+ * @file     /src/routes/index.ts
+ * @version  1.0 
+ * @date     2019-07-09 
+ * @author   Seungjin Bang (seungjin.public＠gmail.com) 
+ * @comment  "REST API 라우팅 취합"
+ */
 import * as Router from 'koa-router'; 
 
-import notice from './notice'; 
+import notice from 'routes/notice'; 
 
-const api = new Router(); 
+const api: Router = new Router(); 
 
 api.use("/notice", notice.routes()); 
 
