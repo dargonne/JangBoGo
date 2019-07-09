917409a47d4c968f0e164837d4b6eb5e1fc3e091
Y2dgWLzz1T <seungjin.public@gmail.com>
2019-07-09 20:31:29 +0900

    공지사항 관련 라우팅

--- jangbogo_backend/src/routes/notice/index.ts
+++ jangbogo_backend/src/routes/notice/index.ts
@@ -1,15 +1,22 @@
+/** 
+ * @project  "JANGBOGO"
+ * @file     /src/routes/notice/index.ts
+ * @version  1.0 
+ * @date     2019-07-09 
+ * @author   Seungjin Bang (seungjin.public＠gmail.com) 
+ * @comment  "공지사항 API 모음"
+ */
+
 import * as Router from 'koa-router'; 
+import NoticeController from 'routes/notice/notice.ctrl'; 
 
-const notice = new Router(); 
+const notice: Router = new Router(); 
+const controller: NoticeController = new NoticeController(); 
 
-notice.get("/", async(ctx, next) => {
-  try {
-    ctx.body = { msg: "Hello notice!"} 
-  } catch (err) {
-    ctx.status = err.status || 500; 
-    ctx.body = err.message; 
-    ctx.app.emit('error', err, ctx); 
-  }
-}); 
+notice.get("/list/:pageNo", controller.getList);
+notice.get("/article/:id", controller.getArticle);
+notice.patch("/article/:id", controller.editArticle);  
+notice.post("/add", controller.addArticle); 
+notice.delete("/drop", controller.dropArticle); 
 
 export default notice; 
