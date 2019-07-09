a3ac5e94dd9a08f2eccf1224d57fea9cd1c973d3
Y2dgWLzz1T <seungjin.public@gmail.com>
2019-07-09 20:30:53 +0900

    서버 실행 코드 변경

--- jangbogo_backend/src/app.ts
+++ jangbogo_backend/src/app.ts
@@ -1,26 +1,39 @@
+/** 
+ * @project  "JANGBOGO"
+ * @file     /src/app.ts
+ * @version  1.0 
+ * @date     2019-07-09 
+ * @author   Seungjin Bang (seungjin.public＠gmail.com) 
+ * @comment  "장보고 서버 시작 코드"
+ */
+
 import * as Koa from "Koa"; 
 import * as Router from "koa-router"; 
-
 import * as logger from "koa-logger"; 
 import * as json from "koa-json"; 
 import * as bodyParser from "koa-bodyparser"; 
 
-import api from './routes'; 
+import SETTINGS from 'settings/index'; 
 
-const app = new Koa(); 
-const router = new Router(); 
+import api from 'routes/index'; 
 
-const PORT = process.env.NODE_ENV === 'development' ? 3000 : 8080; 
+const app: Koa = new Koa(); 
+const router: Router = new Router(); 
 
-// middlewares 
+/** 운영모드에 따라 포트번호 및 서버 설정 지정 */
+const PORT: Number = process.env.NODE_ENV === 'development' ? 3000 : 8080; 
+
+/** 미들웨어 선언 */
 app.use(json()); 
 app.use(logger()); 
-app.use(bodyParser()); 
 
-// routes 
+/** 라우팅 */
 router.use("/api/v1", api.routes()); 
-app.use(router.routes()).use(router.allowedMethods());
+app.use(bodyParser())
+   .use(router.routes())
+   .use(router.allowedMethods());
 
+/** Server Listen */
 app.listen(PORT, () => {
   console.log("*************************************"); 
   console.log(`BACKEND-SERVER MODE : ${process.env.NODE_ENV}`); 
