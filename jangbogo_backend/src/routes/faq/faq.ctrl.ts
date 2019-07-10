9e5f94b551dfa94b9c94fd6898c74d2ff649b54d
Y2dgWLzz1T <seungjin.public@gmail.com>
2019-07-10 23:14:53 +0900

    FAQ 라우팅 컨트롤러

---
+++ jangbogo_backend/src/routes/faq/faq.ctrl.ts
@@ -0,0 +1,88 @@
+/** 
+ * @project  "JANGBOGO"
+ * @file     /src/routes/faq/faq.ctrl.ts
+ * @version  1.0 
+ * @date     2019-07-10
+ * @author   Seungjin Bang (seungjin.public＠gmail.com) 
+ * @comment  "FAQ(자주묻는 질문) 라우트 컨트롤러"
+ */
+ import { Context } from "koa"; 
+
+ import Faq from "models/faq"; 
+
+ export default class FaqController {
+   /** 카테고리별 자주묻는 질문 목록 반환 */
+   async getListByCategory(ctx: Context) {
+     const { category } = ctx.params; 
+
+     let articles; 
+
+     try {
+       articles = await Faq.find({ category }); 
+     } catch(e) {
+       ctx.throw(500, e); 
+     }
+
+     ctx.body = articles; 
+   }
+   
+   /** 신규 자주묻는 질문 글 등록 */
+   async addFaqArticle(ctx: Context) {
+     const { category, title, content } = ctx.request.body; 
+
+     try {
+      const faqArticle = new Faq({
+        category, 
+        title, 
+        content, 
+      }); 
+
+      await faqArticle.save(); 
+     } catch (e) {
+       ctx.throw(500, e); 
+     }
+
+     ctx.body = {
+       "result": true, 
+     }
+   }
+
+   /** 특정 자주묻는 질문 글 수정 */
+   async editFaqArticle(ctx: Context) {
+     const { id } = ctx.params;
+     const { category, title, content } = ctx.request.body;  
+
+     try {
+       await Faq.where({ _id: id })
+                .updateOne({
+                  category, 
+                  title, 
+                  content, 
+                  edited_dt: Date.now() 
+                })
+                .setOptions({ runValidators: true }); 
+
+     } catch (e) {
+       ctx.throw(500, e); 
+     }
+
+     ctx.body = {
+       "result": true, 
+     }
+   }
+
+   /** 특정 자주묻는 질문 글 삭제 */
+   async dropFaqArticle(ctx: Context) {
+    const { id } = ctx.request.body; 
+
+    try {
+      await Faq.findByIdAndRemove(id); 
+    } catch (e) {
+      ctx.throw(500, e); 
+    }
+
+    ctx.body = {
+      "result": true, 
+    }
+   }
+ }
