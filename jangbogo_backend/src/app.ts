f922f02a5837bb2765b57a5606a637fc7aefcc26
Y2dgWLzz1T <seungjin.public@gmail.com>
2019-07-10 23:12:30 +0900

    BACK-END Server Bootstrapping 🚀

--- jangbogo_backend/src/app.ts
+++ jangbogo_backend/src/app.ts
@@ -2,42 +2,19 @@
  * @project  "JANGBOGO"
  * @file     /src/app.ts
  * @version  1.0 
- * @date     2019-07-09 
+ * @date     2019-07-10
  * @author   Seungjin Bang (seungjin.public＠gmail.com) 
- * @comment  "장보고 서버 시작 코드"
+ * @comment  "장보고 서버 Bootstrapping"
  */
 
-import * as Koa from "Koa"; 
-import * as Router from "koa-router"; 
-import * as logger from "koa-logger"; 
-import * as json from "koa-json"; 
-import * as bodyParser from "koa-bodyparser"; 
+import Server from './server'; 
 
-import SETTINGS from 'settings/index'; 
+let SERVER: Server; 
+let PORT: number; 
 
-import api from 'routes/index'; 
+PORT = process.env.NODE_ENV === 'development' ? 3000 : 8080; 
 
-const app: Koa = new Koa(); 
-const router: Router = new Router(); 
-
-/** 운영모드에 따라 포트번호 및 서버 설정 지정 */
-const PORT: Number = process.env.NODE_ENV === 'development' ? 3000 : 8080; 
-
-/** 미들웨어 선언 */
-app.use(json()); 
-app.use(logger()); 
-
-/** 라우팅 */
-router.use("/api/v1", api.routes()); 
-app.use(bodyParser())
-   .use(router.routes())
-   .use(router.allowedMethods());
-
-/** Server Listen */
-app.listen(PORT, () => {
-  console.log("*************************************"); 
-  console.log(`BACKEND-SERVER MODE : ${process.env.NODE_ENV}`); 
-  console.log(`JANGBOGO SERVER RUNNING @ PORT : ${PORT}`); 
-  console.log("*************************************"); 
-}); 
-
+if(PORT) {
+  SERVER = new Server(); 
+  SERVER.listen(PORT); 
+}
